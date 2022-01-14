b8b6a79645351322a998fb19a1dc0e8486e3718c
Larysa Yanushchyk <buha.autilla@gmail.com>
2022-01-14 13:40:52 -0300

    Dropdown Films

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from 'src\data\ghibli\ghibli.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+let AllFilms = data.films;
+console.log(AllFilms);
 
-console.log(example, data);
+// Get dropdown element from DOM
+var dropdownFilms = document.getElementById("selectFilm");
+
+// Loop through the array
+for (var i = 0; i < AllFilms.length; ++i) {
+    // Append the element to the end of Array list
+    dropdownFilms[dropdownFilms.length] = new Option(AllFilms[i].title, AllFilms[i]);
+};
+// console.log()
+// console.log(example, data);
