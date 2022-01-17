3b18e628c50fe8087c508f78a50cebffefff30c8
majesuso <majesus.op@gmail.com>
2022-01-17 17:35:44 -0300

    syntax correction and character filtering

--- src/main.js
+++ src/main.js
@@ -1,28 +1,54 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-let AllFilms = data.films;
-console.log(AllFilms);
+let allFilms = data.films;
+console.log(allFilms);
 
 // Get dropdown element from DOM
 let dropdownFilms = document.getElementById("selectFilm");
 
 // Loop through the array
-for (let i = 0; i < AllFilms.length; ++i) {
+for (let i = 0; i < allFilms.length; ++i) {
     // Append the element to the end of Array list
-    let arrayFilms = AllFilms[i];
-    console.log(arrayFilms);
+    let arrayFilms = allFilms[i];
+    //console.log(arrayFilms);
     dropdownFilms[dropdownFilms.length] = new Option(arrayFilms.title, arrayFilms.id);
+
+    let arrayCharacters = arrayFilms.people;
+    //console.log(arrayCharacters);
 };
 
-dropdownFilms.addEventListener ('change', function () {
+dropdownFilms.addEventListener('change', function () {
     let valueFilm = dropdownFilms.options[dropdownFilms.selectedIndex].value;
-    let resultado = AllFilms.filter(element => element.id == valueFilm);
+    let resultado = allFilms.filter(element => element.id == valueFilm);
     console.log(resultado);
 });
 
+let dropDownGender = document.getElementById("selectGender");
 
-// console.log()
-// console.log(example, data);
+dropDownGender.addEventListener('change', function (showByGender) {
+
+    let allData = data.films,
+        valueCharacter = dropDownGender.value;
+
+    for (let i = 0; i < allData.length; ++i) {
+        let arrayCharacters = allData[i].people;
+        //console.log(arrayCharacters);
+        if (valueCharacter == "Female"){
+            console.log(arrayCharacters.filter(element => element.gender == "Female"))
+        }
+        if (valueCharacter == "Male"){
+            console.log(arrayCharacters.filter(element => element.gender == "Male")) 
+        }
+        if (valueCharacter == "NA"){
+            console.log(arrayCharacters.filter(element => element.gender == "NA"))
+        }
+
+    }
+       /* let arrayCharacters = allCharacter[i];
+        //console.log(arrayCharacters);
+        if (dropDownGender.value == "Female") {
+            console.log(arrayCharacters.filter(element => element.gender == "Female"))
+        }
+        if ()*/
+
+});
