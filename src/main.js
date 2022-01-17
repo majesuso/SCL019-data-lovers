c3d7f45be7cf253859cc78163a8d425a7461dda1
Larysa Yanushchyk <buha.autilla@gmail.com>
2022-01-17 12:13:41 -0300

    filter por id

--- src/main.js
+++ src/main.js
@@ -7,19 +7,20 @@ let AllFilms = data.films;
 console.log(AllFilms);
 
 // Get dropdown element from DOM
-var dropdownFilms = document.getElementById("selectFilm");
+let dropdownFilms = document.getElementById("selectFilm");
 
 // Loop through the array
-for (var i = 0; i < AllFilms.length; ++i) {
+for (let i = 0; i < AllFilms.length; ++i) {
     // Append the element to the end of Array list
     let arrayFilms = AllFilms[i];
-    dropdownFilms[dropdownFilms.length] = new Option(arrayFilms.title, arrayFilms);
+    console.log(arrayFilms);
+    dropdownFilms[dropdownFilms.length] = new Option(arrayFilms.title, arrayFilms.id);
 };
 
 dropdownFilms.addEventListener ('change', function () {
-    var valueFilm = dropdownFilms.options[dropdownFilms.selectedIndex].value;
-    var text = dropdownFilms.options[dropdownFilms.selectedIndex].text;
-    console.log(text);
+    let valueFilm = dropdownFilms.options[dropdownFilms.selectedIndex].value;
+    let resultado = AllFilms.filter(element => element.id == valueFilm);
+    console.log(resultado);
 });
 
 
