bf1e44f2f0bd8677d43091c144c3e5c9fccecb4d
majesuso <majesus.op@gmail.com>
2022-01-20 18:14:58 -0300

    añadir imagen fondo, foto hayao M. y texto reseña

--- src/main.js
+++ src/main.js
@@ -7,15 +7,18 @@ let dropdownFilms = document.getElementById("selectFilm"); // Get dropdown eleme
 for (let i = 0; i < allFilms.length; ++i) {
     // Append the element to the end of Array list
     let arrayFilms = allFilms[i];
-    //console.log(arrayFilms);
+    // la capacidad de dropdownFilms[] se declara según la propiedad title e id del objeto/array, arrayFilms
     dropdownFilms[dropdownFilms.length] = new Option(arrayFilms.title, arrayFilms.id);
 };
 
-dropdownFilms.addEventListener('change', function () {
-    let valueFilm = dropdownFilms.options[dropdownFilms.selectedIndex].value;
-    let filmSelected = allFilms.filter(element => element.id == valueFilm);
+dropdownFilms.addEventListener('change', function (showFilm) {
+    let valueFilm = dropdownFilms.options[dropdownFilms.selectedIndex].value; //valor de Film sera igual al valor del indice seleccionado
+    let filmSelected = allFilms.filter(element => element.id == valueFilm); // Filtra según el id == valor del indice seleccionado
 
+    //Recorriendo array del film seleccionado
     for (let i = 0; i < filmSelected.length; ++i) {
+
+        //Recogiendo propiedades del objeto del film seleccionado
         let filmArray = filmSelected[i],
             titleFilm = filmArray.title,
             descriptionFilm = filmArray.description,
@@ -24,7 +27,8 @@ dropdownFilms.addEventListener('change', function () {
             dateFilm = filmArray.release_date,
             rtScoreFilm = filmArray.rt_score;
 
-        document.write("Título: " + titleFilm  + "<br>" + "Descripción: " + descriptionFilm + "<br>" + "Director: " + directorFilm + "<br>" + "Año: " + dateFilm + "<br>" + rtScoreFilm);
+        document.write("Título: " + titleFilm + "<br>" + "Descripción: " + descriptionFilm + "<br>" + "Director: " + directorFilm + "<br>" + "Año: " + dateFilm + "<br>" + rtScoreFilm);
+
     }
 });
 
